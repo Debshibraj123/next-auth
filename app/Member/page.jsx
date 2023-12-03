7ca5a49055a54a30142dfe0ad9aae7a1bec2814d
Shibraj <shibrajdeb456@gmail.com>
2023-12-03 18:11:20 +0530

    protected member page

--- app/Member/page.jsx
+++ app/Member/page.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 
-const Member = () => {
+const Member = async () => {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/Member");
+  }
+
   return (
     <div>
-       <h1>Member</h1>
+      <h1>Member Server Session</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.role}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Member
+export default Member;
