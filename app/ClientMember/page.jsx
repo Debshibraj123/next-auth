8400f455b1d5bc38e3971d9d53ba7e1640cc535e
Shibraj <shibrajdeb456@gmail.com>
2023-12-03 18:00:33 +0530

    first commit

---
+++ app/ClientMember/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+const ClientMember = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ClientMember
---
+++ app/Public/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Public = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Public
